5249d2d1e4616874458cc388f51931c5ae9387f6
mahi7000 <mahlet7000@gmail.com>
2024-10-03 13:58:38 +0000

    pass lint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = array;
   for (const value of array) {
     const idx = array.indexOf(value);
-    array[idx] = appendString + value;
+    newArray[idx] = appendString + value;
   }
 
-  return array;
+  return newArray;
 }
