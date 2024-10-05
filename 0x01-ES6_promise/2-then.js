3e7ff50af34e1978608267e6338f4352c5b9a69f
mahi7000 <mahlet7000@gmail.com>
2024-10-05 08:43:31 +0000

    then, catch and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => new Error());
+  const resolves = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => resolves)
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
